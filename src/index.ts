126c4cd50887480f3618207819aca2d195cb4b89
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-03-02 16:28:55 +0100

    create SinglyLinkedList

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,33 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val: any) {
+    const newNode = new _Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else if (this.tail) {
+      this.head.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  traverse() {
+    let current = this.head;
+    while (current !== null) {
+      console.log(current);
+      current = current.next;
+    }
+  }
 }
 
 const list = new SinglyLinkedList();
-
-const first = new _Node('Hi');
-first.next = new _Node('Im next node!');
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(2);
+list.push(3);
+list.traverse(); // prints 1 2 3
