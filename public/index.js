7c497755165a8fb46f0857d5916c2df57bacc66f
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-03-03 01:13:16 +0100

    get  method

--- public/index.js
+++ public/index.js
@@ -44,9 +44,48 @@ class SinglyLinkedList {
         }
         return this;
     }
+    shift() {
+        if (!this.head)
+            return undefined;
+        const currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return currentHead;
+    }
+    unshift(val) {
+        const newNode = new _Node(val);
+        if (!this.head)
+            return this.push(val);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+    get(ind) {
+        if (ind < 0 || ind >= this.length || this.head === null)
+            return null;
+        let currentNode = this.head;
+        for (let i = 0; i <= ind; i++) {
+            if (i === ind) {
+                return currentNode;
+            }
+            else {
+                if (currentNode === null)
+                    return null;
+                currentNode = currentNode.next;
+            }
+        }
+        return currentNode;
+    }
 }
 const list = new SinglyLinkedList();
 list.push(1);
-list.push(2);
+list.push('A');
 list.push(3);
+list.push('B');
+list.push(2);
+list.push('C');
 //# sourceMappingURL=index.js.map
